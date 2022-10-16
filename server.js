9648863f2604a25064f5cd207535d0907d8ad6c8
Jaewoo Han <jhan2021@g.ucla.edu>
2022-10-16 06:15:05 -0700

    delete files

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
 // For Contact Form
 // Must add business email running on server
 
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+// const express = require("express");
+// const router = express.Router();
+// const cors = require("cors");
+// const nodemailer = require("nodemailer");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(3001, () => console.log("Server Running"));
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", router);
+// app.listen(3001, () => console.log("Server Running"));
 
-const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: "***************@gmail.com",
-      pass: "********",
-    },
-  });
+// const contactEmail = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: "***************@gmail.com",
+//       pass: "********",
+//     },
+//   });
   
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+//   contactEmail.verify((error) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log("Ready to Send");
+//     }
+//   });
 
-  router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message; 
-    const mail = {
-      from: name,
-      to: "***************@gmail.com",
-      subject: "Contact Form Submission",
-      html: `<p>Name: ${name}</p>
-             <p>Email: ${email}</p>
-             <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-      if (error) {
-        res.json({ status: "ERROR" });
-      } else {
-        res.json({ status: "Message Sent" });
-      }
-    });
-  });
+//   router.post("/contact", (req, res) => {
+//     const name = req.body.name;
+//     const email = req.body.email;
+//     const message = req.body.message; 
+//     const mail = {
+//       from: name,
+//       to: "***************@gmail.com",
+//       subject: "Contact Form Submission",
+//       html: `<p>Name: ${name}</p>
+//              <p>Email: ${email}</p>
+//              <p>Message: ${message}</p>`,
+//     };
+//     contactEmail.sendMail(mail, (error) => {
+//       if (error) {
+//         res.json({ status: "ERROR" });
+//       } else {
+//         res.json({ status: "Message Sent" });
+//       }
+//     });
+//   });
